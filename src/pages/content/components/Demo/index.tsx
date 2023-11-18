7cc9bab6498273f0edf61a4504e9b57582ef7c00
2coolchampion <franvidicek@gmail.com>
2023-11-18 21:36:05 +0100

    Position outer root instead of inner shadow one

--- src/pages/content/components/Demo/index.tsx
+++ src/pages/content/components/Demo/index.tsx
@@ -11,11 +11,16 @@ root.style.all = "unset";
 
 document.body.append(root);
 
+root.style.zIndex = '9999'
+root.style.position = "fixed";
+root.style.bottom = "0";
+root.style.right = "0";
+
 const rootIntoShadow = document.createElement("div");
 rootIntoShadow.id = "shadow-root";
-rootIntoShadow.style.position = "fixed";
-rootIntoShadow.style.bottom = "0";
-rootIntoShadow.style.right = "0";
+// rootIntoShadow.style.position = "fixed";
+// rootIntoShadow.style.bottom = "0";
+// rootIntoShadow.style.right = "0";
 
 const shadowRoot = root.attachShadow({ mode: "open" });
 shadowRoot.appendChild(rootIntoShadow);
