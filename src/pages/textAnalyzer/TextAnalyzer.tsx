fb48751acd919dabfcf3dd91ddf73a6ada22fe2f
2coolchampion <franvidicek@gmail.com>
2023-11-18 21:37:59 +0100

    Tweak textAnalyzer basics

--- src/pages/textAnalyzer/TextAnalyzer.tsx
+++ src/pages/textAnalyzer/TextAnalyzer.tsx
@@ -1,6 +1,55 @@
 
+
 const TextAnalyzer = () => {
-  return <div>TextAnalyzer</div>;
+
+  const handleSendToFastAPI = () => {
+    const textBox = document.getElementById("textbox");
+    const data = textBox.innerText;
+
+    fetch("http://127.0.0.1:8000/process", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ data: data }), // send data as JSON object with key "data"
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data.processed_data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
+
+  return (
+    <>
+      <div 
+      className="flex flex-col justify-center items-center w-full h-full mt-10"
+      >
+        <div 
+        id="textbox"
+        contentEditable
+        className="border-1 border-black p-14 w-1/2 mb-10"
+
+        >
+        </div>
+        <div
+        className="flex justify-between w-1/2"
+        >
+          <button 
+          className="text-sm p-1 border-1 border-black hover:bg-gray-200"
+          onClick={handleSendToFastAPI}
+          >send to FastAPI
+          </button>
+          <button
+          className="text-sm p-1 border-1 border-black hover:bg-gray-200"
+          >Manual mode
+          </button>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default TextAnalyzer;
--- src/pages/textAnalyzer/index.tsx
+++ src/pages/textAnalyzer/index.tsx
@@ -1,18 +1,18 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import "@pages/popup/index.css";
+import "@pages/textAnalyzer/index.css";
 import TextAnalyzer from "@root/src/pages/textAnalyzer/TextAnalyzer";
 import refreshOnUpdate from "virtual:reload-on-update-in-view";
 import { attachTwindStyle } from "@src/shared/style/twind";
 
-refreshOnUpdate("pages/popup");
+refreshOnUpdate("src/pages/textAnalyzer");
 
 function init() {
   const appContainer = document.querySelector("#app-container");
+  attachTwindStyle(appContainer, document);
   if (!appContainer) {
     throw new Error("Can not find #app-container");
   }
-  attachTwindStyle(appContainer, document);
   const root = createRoot(appContainer);
   root.render(<TextAnalyzer />);
 }
