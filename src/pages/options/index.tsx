890c74e5b71f6188f88123e4872e2f16003dda8a
2coolchampion <franvidicek@gmail.com>
2023-12-13 16:40:21 +0100

    Remove Twind from project

--- src/pages/options/index.tsx
+++ src/pages/options/index.tsx
@@ -3,16 +3,15 @@ import { createRoot } from "react-dom/client";
 import Options from "@pages/options/Options";
 import "@pages/options/index.css";
 import refreshOnUpdate from "virtual:reload-on-update-in-view";
-import { attachTwindStyle } from "@root/src/shared/style/twind";
 
 refreshOnUpdate("pages/options");
 
 function init() {
   const appContainer = document.querySelector("#app-container");
-  attachTwindStyle(appContainer, document);
   if (!appContainer) {
     throw new Error("Can not find #app-container");
   }
+
   const root = createRoot(appContainer);
   root.render(<Options />);
 }
--- src/pages/textAnalyzer/index.tsx
+++ src/pages/textAnalyzer/index.tsx
@@ -3,16 +3,15 @@ import { createRoot } from "react-dom/client";
 import "@pages/textAnalyzer/index.css";
 import TextAnalyzer from "@root/src/pages/textAnalyzer/TextAnalyzer";
 import refreshOnUpdate from "virtual:reload-on-update-in-view";
-import { attachTwindStyle } from "@src/shared/style/twind";
 
 refreshOnUpdate("src/pages/textAnalyzer");
 
 function init() {
   const appContainer = document.querySelector("#app-container");
-  attachTwindStyle(appContainer, document);
   if (!appContainer) {
     throw new Error("Can not find #app-container");
   }
+  
   const root = createRoot(appContainer);
   root.render(<TextAnalyzer />);
 }
